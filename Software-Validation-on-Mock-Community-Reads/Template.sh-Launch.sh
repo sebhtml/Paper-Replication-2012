d438d3df1a4efeb3f27b09ac80dedf588836f086
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-10-02 23:15:43 -0400

    I added the scripts for mock communities.
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

---
+++ Software-Validation-on-Mock-Community-Reads/Template.sh-Launch.sh
@@ -0,0 +1,5 @@
+
+qsub Template.sh-SRX030841.sh
+qsub Template.sh-SRX030842.sh
+qsub Template.sh-SRX055380.sh
+qsub Template.sh-SRX055381.sh
---
+++ Software-Validation-on-Mock-Community-Reads/Template.sh-SRX030841.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#PBS -N SRX030841.Ray-Communities-2012-10-02.2
+#PBS -q qwork@mp2
+#PBS -l nodes=3:ppn=1
+#PBS -l walltime=48:00:00
+cd $PBS_O_WORKDIR
+
+. /mnt/scratch_mp2/corbeil/corbeil_group/software/NGS-Pipelines/LoadModules.sh
+
+root=/mnt/scratch_mp2/corbeil/corbeil_group/genomes/
+
+mpiexec -n 72 Ray \
+ -o \
+ SRX030841.Ray-Communities-2012-10-02.2 \
+ -k \
+ 31 \
+ -s SRX030841.fastq.gz \
+  -search \
+ $root/EMBL_CDS+GO/EMBL_CDS_Sequences \
+ -gene-ontology \
+ $root/EMBL_CDS+GO/000-Ontologies.txt \
+ $root/EMBL_CDS+GO/000-Annotations.txt \
+ -search \
+ $root/RayKmerSearchStuff/last-build/ARDB \
+ -search \
+ $root/RayKmerSearchStuff/last-build/Bacteria-Genomes \
+ -search \
+ $root/RayKmerSearchStuff/last-build/HumanChromosomes \
+ -search \
+ $root/RayKmerSearchStuff/last-build/NCBI-Bacteria_DRAFT \
+ -search \
+ $root/RayKmerSearchStuff/last-build/Viruses-Genomes \
+ -with-taxonomy \
+ $root/taxonomy/last-build/Genome-to-Taxon.tsv \
+ $root/taxonomy/last-build/TreeOfLife-Edges.tsv \
+ $root/taxonomy/last-build/Taxon-Names.tsv
+
+
