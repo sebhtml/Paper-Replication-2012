b1d7a4d5fc04fda93a0880f8b357bafb7dd2f17a
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-10-04 23:13:39 -0400

    I added files to validate the 100 genome meta assembly.
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

---
+++ Validation-of-RayMeta-100g/Validate-Assembly.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+TEST_NAME=Sample-100.RayOutput
+
+ValidateGenomeAssembly.sh reference-metagenome.fasta $TEST_NAME/Contigs.fasta RayMeta
+
+
---
+++ Validation-of-RayMeta-100g/load-programs.sh
@@ -0,0 +1,4 @@
+
+export PATH=$(pwd)/scripts:$PATH
+
+. /software/MUMmer/3.23/MUMmer3.23/Load.sh
