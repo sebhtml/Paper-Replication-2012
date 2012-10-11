c29d6c4166a5b4a7e4435af864b11012f5d52a5c
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-10-11 16:22:15 -0400

    Added results for mock communities.
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

---
+++ Software-Validation-on-Mock-Community-Reads/Build-Table.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+class Table:
+	def __init__(self,expectedResults,illuminaResults,the454Results):
+		self.
+
+even=Table("Even.Expected.txt","Even.Actual-Illumina.txt","Even.Actual-454.txt")
+
+staggered=Table("Staggered.Expected.txt","Staggered.Actual-Illumina.txt","Staggered.Actual-454.txt")
