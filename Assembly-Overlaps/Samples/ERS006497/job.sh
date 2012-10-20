07f83125340d92b2ec6c99aa592332d952660f6a
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-10-19 20:20:19 -0400

    added scripts to filter scaffolds.
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- Assembly-Overlaps/Samples/ERS006497/job.sh
+++ Assembly-Overlaps/Samples/ERS006497/job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -N ERS006497-overlaps-2012-10-06.1
-#PBS -o ERS006497-overlaps-2012-10-06.1.stdout
-#PBS -e ERS006497-overlaps-2012-10-06.1.stderr
+#PBS -N ERS006497-overlaps-2012-10-14.1
+#PBS -o ERS006497-overlaps-2012-10-14.1.stdout
+#PBS -e ERS006497-overlaps-2012-10-14.1.stderr
 #PBS -A nne-790-ab
 #PBS -l walltime=48:00:00
 #PBS -l nodes=12:ppn=8
@@ -10,11 +10,11 @@ cd $PBS_O_WORKDIR
 source /rap/nne-790-ab/software/NGS-Pipelines/LoadModules.sh
 
 mpiexec -n 96 \
--output-filename ERS006497-overlaps-2012-10-06.1 \
+-output-filename ERS006497-overlaps-2012-10-14.1 \
 Ray \
 -k 31 \
--o ERS006497-overlaps-2012-10-06.1 \
--search Assemblies \
+-o ERS006497-overlaps-2012-10-14.1 \
+-search Assemblies.500 \
 -p Reads/ERR011099_1.fastq.gz Reads/ERR011099_2.fastq.gz \
 -p Reads/ERR011100_1.fastq.gz Reads/ERR011100_2.fastq.gz \
 -p Reads/ERR011101_1.fastq.gz Reads/ERR011101_2.fastq.gz \
@@ -23,4 +23,4 @@ Ray \
 -p Reads/ERR011104_1.fastq.gz Reads/ERR011104_2.fastq.gz \
 -one-color-per-file
 
-mv ERS006497-overlaps-2012-10-06.1.* ERS006497-overlaps-2012-10-06.1
+mv ERS006497-overlaps-2012-10-14.1.* ERS006497-overlaps-2012-10-14.1
